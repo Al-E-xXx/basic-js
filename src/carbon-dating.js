2a172014ffdde2168569fc2ca305739b00680060
Alex <asgaroth@yandex.ru>
2023-04-13 01:49:56 +0300

    feat: add carbon-dating (04/13/2023 01:49:55)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+      typeof sampleActivity !== 'string' ||
+      Number(sampleActivity) > 15 ||
+      Number(sampleActivity) <= 0 ||
+      isNaN(Number(sampleActivity))
+      ) {
+    return false;
+  }
+
+  return Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity)) * (HALF_LIFE_PERIOD)) / Math.log(2));
 }
 
 module.exports = {
