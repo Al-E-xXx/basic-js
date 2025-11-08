20a1131bce3bfce09c8f2f24fda49cc82d7923df
alex <vurdalackoff@gmail.com>
2025-11-08 14:40:46 +0300

    feat: dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,31 @@ const { NotImplementedError } = require('../lib');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+
+function getDNSStats(domains) {
+  let domMatrix = [];
+  let resp = {};
+  let domName = "";
+
+  for (let i = 0; i < domains.length; i++) {
+    domMatrix[i] = domains[i].split(".").reverse();
+  }
+
+  for (let i = 0; i < domMatrix.length; i++) {
+    domName = "";
+    for (let k = 0; k < domMatrix[i].length; k++) {
+      domName += "." + domMatrix[i][k];
+      if (domName in resp) {
+        resp[domName] = resp[domName] + 1;
+      } else {
+        resp[domName] = 1;
+      }
+    }
+  }
+
+  return resp;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
