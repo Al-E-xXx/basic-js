c0873803cb9d5858c79d5f8eb5c6f6d31964d7d4
alex <vurdalackoff@gmail.com>
2025-11-09 02:29:57 +0300

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -19,15 +19,145 @@ const { NotImplementedError } = require('../lib');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direction) {
+    if (direction === false) {
+      this.reverse = true;
+    }
+
+    this.ALPH = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const strWithoutSpaces = message.split(/\s+/).join("");
+    let spaceIndx = [];
+    let resultArr = [];
+    let keyCnt = 0;
+    let newSymbolInd;
+    let newSymbol;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " ") {
+        spaceIndx.push(i);
+      }
+    }
+
+    for (let i = 0; i < strWithoutSpaces.length; i++) {
+      if (this.ALPH.includes(strWithoutSpaces[i])) {
+        newSymbolInd =
+          (this.ALPH.indexOf(strWithoutSpaces[i]) +
+            this.ALPH.indexOf(key[keyCnt])) %
+          26;
+        newSymbol = this.ALPH[newSymbolInd];
+
+        resultArr.push(newSymbol);
+
+        if (keyCnt === key.length - 1) {
+          keyCnt = 0;
+        } else {
+          keyCnt++;
+        }
+      } else {
+        resultArr.push(strWithoutSpaces[i]);
+      }
+    }
+
+    for (let i = 0; i < spaceIndx.length; i++) {
+      resultArr.splice(spaceIndx[i], 0, " ");
+    }
+
+    if (this.reverse) {
+      resultArr.reverse();
+    }
+
+    return resultArr.join("");
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const strWithoutSpaces = message.split(/\s+/).join("");
+    let spaceIndx = [];
+    let resultArr = [];
+    let keyCnt = 0;
+    let newSymbol;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] === " ") {
+        spaceIndx.push(i);
+      }
+    }
+
+    for (let i = 0; i < strWithoutSpaces.length; i++) {
+      if (this.ALPH.includes(strWithoutSpaces[i])) {
+        let newSymbolInd =
+          this.ALPH.indexOf(strWithoutSpaces[i]) -
+          this.ALPH.indexOf(key[keyCnt]);
+
+        if (newSymbolInd < 0) {
+          newSymbolInd = newSymbolInd + 26;
+        }
+
+        newSymbol = this.ALPH[newSymbolInd];
+        resultArr.push(newSymbol);
+
+        if (keyCnt === key.length - 1) {
+          keyCnt = 0;
+        } else {
+          keyCnt++;
+        }
+      } else {
+        resultArr.push(strWithoutSpaces[i]);
+      }
+    }
+
+    for (let i = 0; i < spaceIndx.length; i++) {
+      resultArr.splice(spaceIndx[i], 0, " ");
+    }
+
+    if (this.reverse) {
+      resultArr.reverse();
+    }
+
+    return resultArr.join("");
   }
 }
 
